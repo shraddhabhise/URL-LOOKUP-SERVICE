e34e2e36833d2ad1f89a34545fcd1a7adfac4751
shraddha bhise <shraddhabhise838@gmail.com>
2021-08-28 23:31:51 -0400

    Added logic to validate url

--- api/api-file.py
+++ api/api-file.py
@@ -17,6 +17,24 @@ DATABASE = "/Users/shraddhabhise/PycharmProjects/URL-LOOKUP-SERVICE/api/database
 def home():
     return "<h1>Welcome</h1>"
 
+
+@app.route('/list')
+def list():
+    con = sqlite3.connect("/Users/shraddhabhise/PycharmProjects/URL-LOOKUP-SERVICE/api/database.db")
+    con.row_factory = sqlite3.Row
+
+    cur = con.cursor()
+    cur.execute("select * from malwarehosts")
+
+    rows = cur.fetchall();
+
+    return render_template("list.html", rows=rows)
+
+@app.route('/addmalware')
+def new_malware():
+   return render_template('malware.html')
+
+
 @app.route('/add', methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
@@ -37,22 +55,62 @@ def add():
         return render_template("result.html", msg=msg)
 
 
-@app.route('/list')
-def list():
-    con = sqlite3.connect("/Users/shraddhabhise/PycharmProjects/URL-LOOKUP-SERVICE/api/database.db")
-    con.row_factory = sqlite3.Row
-
-    cur = con.cursor()
-    cur.execute("select * from malwarehosts")
-
-    rows = cur.fetchall();
-
-    return render_template("list.html", rows=rows)
+@app.route('/checkurl')
+def new_url():
+   return render_template("checkurl.html")
 
 
-@app.route('/addmalware')
-def new_malware():
-   return render_template('malware.html')
+@app.route('/isvalid',  methods=['POST', 'GET'])
+def isvalid():
+    if request.method == 'POST':
+        ur = request.form['url']
+        isVALID=False
+        isPresent=False
+
+        regex = re.compile(
+            r'^(?:http|ftp)s?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+            r'localhost|'  # localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        match = re.search(regex, ur)
+        if not match:
+            msg= "malformed url. Enter proper format of url"
+
+        else:
+            hostname=match.group().strip(r'^(?:http|ftp)s?://')
+            print("hostname", hostname)
+
+            conn = sqlite3.connect("/Users/shraddhabhise/PycharmProjects/URL-LOOKUP-SERVICE/api/database.db")
+
+            #check/search  in database and change ispresent flag
+            print("Opened database successfully for searching malware")
+            try:
+                with conn:
+                    conn.row_factory = sqlite3.Row
+                    cur = conn.cursor()
+                    cur.execute("select * from malwarehosts where url=?", (hostname,))
+                    rows= cur.fetchone()
+                    if rows:
+                        isPresent=True
+
+            except sqlite3.IntegrityError:
+                    print("Database operation failed")
+            conn.close()
+
+
+            if isPresent==False:
+                msg="Safe and valid URL. You can proceed to use it."
+            else:
+                msg = "Malware URL and is not safe to use"
+
+
+
+    return render_template("result.html", msg=msg)
+
+
 
 if __name__ == '__main__':
     app.run()
